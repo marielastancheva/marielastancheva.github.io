512f76eff787dfb8b00505a4662f2bd52ffa2965
marielastancheva <mariela_stan4eva@abv.bg>
2014-05-30 20:05:02 +0300

    Hash generation from file

--- myRuby.rb
+++ myRuby.rb
@@ -1,46 +1,9 @@
 #encoding: utf-8
-templ='	<!DOCTYPE html>
-<html>
-<head>
-	<title>title1</title>
-		<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles/style.css\">
-
-</head>
-<body>
-	<h1>Мариела</h1>
-
-	<article 
-		class=\"main\" 
-		title=%{title1}
-	>
-		<p>%{paragraph1}</p>
-		<p>%{paragraph2}</p>
-		<p>%{paragraph3}</p>
-		<p>%{paragraph4}</p>
-			<a href=\"../en/index.html\">english</a>
-	</article>
-</body>
-</html>'
-
-bgText = {
-:title1=> "My page title bg",
-:paragraph1=> "Нещо за мен",
-:paragraph2=> "Още нещо за мен",
-:paragraph3=> "И още нещо",
-:paragraph4=> "И още едно нещо" }
-
-enText = {
-:title1=> "Моята страница en",
-:paragraph1=> "Something for me",
-:paragraph2=> "Another thing for me",
-:paragraph3=> "And another",
-:paragraph4=> "And another else thing for me" }
-
-templ1=templ%bgText
+require 'yaml'
+templ=IO.read('templ.html')
+bgText = YAML.load_file('bg_hash.yml')
+enText = YAML.load_file('en_hash.yml')
 templ2=templ%enText
-
-
-IO.write("testfilebg", templ1) 
-IO.write("testfileen", templ2)
-
-
+templ1=templ%bgText
+IO.write("testfilebg.html", templ1) 
+IO.write("testfileen.html", templ2)
